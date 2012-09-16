569c5b711c6ec580fe11fc0f4a6468ecab04ab38
jodyalbritton <jody.albritton@gmail.com>
2012-09-16 06:38:24 -0500

    Init the repository 	new file:   .gitignore 	new file:   .project 	new file:   Gemfile 	new file:   Gemfile.lock 	new file:   README.rdoc 	new file:   Rakefile 	new file:   app/assets/images/favicon.ico 	new file:   app/assets/images/rails.png 	new file:   app/assets/javascripts/application.js 	new file:   app/assets/javascripts/bootstrap.js.coffee 	new file:   app/assets/javascripts/welcome.js.coffee

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Incept::Application.config.secret_token = '861a9edbbb5286b0a9acc96cf8cf76f41a3822cca0fe4a4ba577bdf371f354ce71bf9f2c8ccf5324cb9dbb9212367e14ad94fb17521866ea639d684d15a222b6'
