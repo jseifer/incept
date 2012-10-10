8e497bd1b2bed77ffde94a7c34b7e767004c36a3
jodyalbritton <jody.albritton@gmail.com>
2012-10-10 05:44:47 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid "/var/src/incept/tmp/pids/unicorn.pid"
 stderr_path "/var/src/incept/log/unicorn.log"
 stdout_path "/var/src/incept/log/unicorn.log"
 
-listen "/tmp/unicorn.incept.sock"
+listen "/tmp/unicorn.incept.sock", :backlog => 64
 worker_processes 2
 timeout 30
