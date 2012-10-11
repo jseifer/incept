b62a7d6c5f4403bb82c50af260f430182751c5e9
jodyalbritton <jody.albritton@gmail.com>
2012-10-10 20:16:46 -0500

    settings

--- config/incept_init.sh
+++ config/incept_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/src/incept
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/incept_unicorn.rb -E production"
+CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/incept_unicorn.rb -E development"
 action="$1"
 set -u
 
