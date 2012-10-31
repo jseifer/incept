24d756575a3a85fd6757d7b0a9f13cc4500b37bb
jodyalbritton <jody.albritton@gmail.com>
2012-10-31 13:15:38 -0500

    fix one time tasks list

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     @user = User.find(current_user)
     @task_completion = @user.task_completions.new
     @todo_list = @user.task_ownerships.where(:reccuring => true)
-    @task_list = @user.task_ownerships.where(:reccuring => false, :complete => false)
+    @task_list = @user.task_ownerships.where("reccuring = false", "complete = false")
     @complete = @user.task_completions.where('DATE(completed_at) = ?', Date.today)
   end
 end
